24ef5c76bfb9df87976fcab3b7f2b8205435e965
yohanduartep <yohandpessanha@gmail.com>
2025-04-27 11:06:13 -0300

    Adding nvim

---
+++ nvim/lua/yohansan1/lazy/init.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+    "eandrju/cellular-automaton.nvim",
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+}
+
